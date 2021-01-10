f9d9daea838a66733fa39ef8d2c09db29684993b
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-01-10 15:10:32 +0530

    completed Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return ( <
+        header >
+        <
+        h1 > The < strong > Anime < /strong>Database</h
+        1 >
+        <
+        /header>
+    )
+}
+
+export default Header
